d5b22025d92b293f3b4ef59c8b0a9535cc396474
haduigon <haduigon@gmail.com>
2022-07-02 15:45:31 +0300

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,30 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/Entire_document.png",
+        "test": "../bitmaps_test/20220702-154222/Entire_document.png",
+        "selector": "document",
+        "fileName": "Entire_document.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/hello-world/",
+        "expect": 0,
+        "viewportLabel": "custom-size",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -872
+          },
+          "misMatchPercentage": "0.82",
+          "analysisTime": 22
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
